860182e7f34ecf99a3351e8ca07df576afb6b272
nobodyzxc <dici86.chen@gmail.com>
2017-11-25 17:53:20 +0800

    first commit

---
+++ Assets/smoking.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class smoking : MonoBehaviour {
+	int life = 500;
+	// Use this for initialization
+	void Start () {
+		this.GetComponent<ParticleSystem> ().Play ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(this.GetComponent<Light> ().intensity > 0)
+			this.GetComponent<Light> ().intensity -= 0.5f;
+		
+		if (life > 0)
+			life -= 1;
+		if(life == 350)
+			this.GetComponent<ParticleSystem> ().Stop ();
+		if (life == 100)
+			Destroy (gameObject);
+	}
+}
---
+++ Assets/spinCannon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spinCannon : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float Xangle = transform.rotation.eulerAngles.x;
+		if (Xangle > 180)
+			Xangle -= 360;
+		
+		if (Input.GetKey (KeyCode.A))
+			gameObject.transform.parent.transform.Rotate (new Vector3 (0, -1, 0));
+		if (Input.GetKey (KeyCode.D))
+			gameObject.transform.parent.transform.Rotate (new Vector3 (0, 1, 0));
+		if (Input.GetKey (KeyCode.W) && Xangle < 72.5f)
+			gameObject.transform.Rotate (new Vector3 (1, 0, 0));
+		if (Input.GetKey (KeyCode.S) && Xangle > -6.5f)
+			gameObject.transform.Rotate (new Vector3 (-1, 0, 0));
+
+	}
+}

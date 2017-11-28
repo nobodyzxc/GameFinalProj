867b8f69ec058bb0fd83e7456cb92564934efd6b
nobodyzxc <dici86.chen@gmail.com>
2017-11-28 13:05:23 +0800

    ckp 1

---
+++ Assets/humanAct.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class humanAct : MonoBehaviour {
+
+	Rigidbody rbody;
+	// Use this for initialization
+	void Start () {
+		rbody = gameObject.GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print (rbody.velocity);
+	}
+}

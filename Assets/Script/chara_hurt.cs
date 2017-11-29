74b0aeb237b571d8bbc3118d767dd33e89a05444
sleepingfish <0487iamapig@gmail.com>
2017-11-29 15:51:16 +0800

    haven't add the bleeding effect yet

---
+++ Assets/Script/chara_hurt.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chara_hurt : MonoBehaviour {
+
+	void OnJointBreak(float bf){
+		GetComponent<Renderer> ().material.color = new Color (1, 0, 0, 1);
+		GetComponent<Joint>().connectedBody.gameObject.GetComponent<Renderer> ().material.color = new Color (1, 0, 0, 1);
+	}
+}

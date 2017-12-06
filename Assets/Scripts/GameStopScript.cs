f561ae3a29164f58ed289b20f4c0d9c09c44e2ff
Bin-Jing <jeff20001234@yahoo.com.tw>
2017-12-06 18:25:59 +0800

    add game pause and start button

---
+++ Assets/Scripts/GameStopScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStopScript : MonoBehaviour {
+	public GameObject PauseButton;
+	public GameObject StartButton;
+
+	void Start(){
+		PauseButton.SetActive (true);
+		StartButton.SetActive (false);
+	}
+	public void GamePauseButton(){
+		if (Time.timeScale == 1) {
+			PauseButton.SetActive (false);
+			StartButton.SetActive (true);
+			Time.timeScale = 0;
+		} else {
+			PauseButton.SetActive (true);
+			StartButton.SetActive (false);
+			Time.timeScale = 1;
+		}
+	}
+}

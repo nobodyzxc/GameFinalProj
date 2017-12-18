70b8e5ec42fb6e88215f3c202143cfdf9c6d1246
nobodyzxc <dici86.chen@gmail.com>
2017-12-19 00:47:29 +0800

    update

---
+++ Assets/rtnHeight.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class rtnHeight : MonoBehaviour {
+
+	public Slider slider;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		slider.value = Mathf.Clamp(transform.position.y, slider.minValue, slider.maxValue);
+	}
+}

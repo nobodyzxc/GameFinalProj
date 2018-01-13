1a580512c27df838efc9eb390daafa85d0943849
Bin-Jing <jeff20001234@yahoo.com.tw>
2018-01-13 18:51:46 +0800

    add cloud

---
+++ Assets/Scripts/GenerateCloudScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateCloudScript : MonoBehaviour {
+
+	public GameObject cloud;
+	public float spawnMin = 1f;
+	public float spawnMax = 2f;
+	public jump Player;
+	// Use this for initialization
+	void Start () {
+		Spawn ();
+	}
+
+	void Spawn(){
+		if (Player.state == 2) {
+			Destroy(Instantiate (cloud, transform.position, Quaternion.identity),3);
+
+		}
+		Invoke ("Spawn", Random.Range (spawnMin, spawnMax));
+
+	}
+}
